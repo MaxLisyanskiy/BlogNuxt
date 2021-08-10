e3ffb698968109a8754bca7a7d666948197f786d
Maksim Lisianskii <Max_Lisyanskiy@mail.ru>
2021-08-10 20:27:09 +0300

    add: commets for newcommets

--- components/Comments/NewComment.vue
+++ components/Comments/NewComment.vue
@@ -19,6 +19,12 @@
 
 <script>
 export default {
+    props: {
+        postId: {
+            type: String,
+            required: true
+        }
+    },
     data() {
         return {
             message: null,
@@ -31,7 +37,7 @@ export default {
     methods: {
         onSubmit () {
             this.$store.dispatch('addComment', {
-                postId: '',
+                postId: this.postId,
                 publish: false,
                 ...this.comment
             })
@@ -49,7 +55,7 @@ export default {
 <style lang="scss">
     .new-comment {
         text-align: center;
-        background-color: #afffa4;
+        background-color: #676966;
         &__form {
             max-width: 600px;
             margin: 30px auto;
--- pages/blog/_id/index.vue
+++ pages/blog/_id/index.vue
@@ -2,7 +2,7 @@
   <div class="wrapper-content wrapper-content--fixed">
       <Post :post="post" />
       <Comments :comments="comments" />
-      <NewComment />
+      <NewComment :postId="$route.params.id" />
   </div>
 </template>
 
@@ -23,9 +23,21 @@ export default {
             axios.get(`https://blog-nuxt-67ba8-default-rtdb.europe-west1.firebasedatabase.app/posts/${context.params.id}.json`),
             axios.get(`https://blog-nuxt-67ba8-default-rtdb.europe-west1.firebasedatabase.app/comments.json`)
         ])
+        // let commentsArray = [],
+        //     commentsArrayRes = []
+        // Object.keys(comments.data).forEach(key => {
+        //     commentsArray.push(comments.data[key])
+        // })
+        // for (let i=0; i < commentsArray.length; i++) {
+        //     if(commentsArray[i].postId === context.params.id && commentsArray[i].publish === true) {
+        //         commentsArrayRes.push(commentsArray[i])
+        //     }
+        // }
+        let commentsArrayRes = Object.values(comments.data).filter( comment => (comment.postId === context.params.id) && comment.publish)
+
         return {
             post: post.data,
-            comments: comments.data
+            comments: commentsArrayRes
         }
     }
 }
